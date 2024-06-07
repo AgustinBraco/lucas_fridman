894d8e8c2626b47416f08757fe91154e806899f8
Agustin Braco <bracoagustin@gmail.com>
2024-06-06 21:14:16 -0300

    [Main] - Full screen image with click and background pattern

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,13 +1,29 @@
 const galleryWrapper = document.querySelector('.GalleryWrapper');
 
 let galleryHandler = '';
-let length =  33;
+let length = 33;
 
-for (let i = 1; i <= length; i++)
+for (let i = 1; i <= length; i++) {
   galleryHandler += `
-    <div class="GalleryImageWrapper">
-      <img class="GalleryImage" src="public/gallery/gallery_${i}.jpg" alt="imagen">
+    <div class="GalleryImageWrapper ImageWrapper${i}">
+      <img class="GalleryImage Image${i}" src="public/gallery/gallery_${i}.jpg" alt="imagen">
     </div>
   `;
+}
 
 galleryWrapper.innerHTML = galleryHandler;
+
+for (let i = 1; i <= length; i++) {
+  const imageWrapper = document.querySelector(`.ImageWrapper${i}`);
+  const image = document.querySelector(`.Image${i}`);
+
+  imageWrapper.addEventListener('click', () => {
+    if (imageWrapper.classList.contains('wrapperFull')) {
+      imageWrapper.classList.remove('wrapperFull');
+      image.classList.remove('imageFull');
+      return;
+    };
+    imageWrapper.classList.add('wrapperFull');
+    image.classList.add('imageFull');
+  });
+};
